e30321f189e221c292be612db239e6e25f5fe29b
Nils Krüger <rad@rad.vg>
2026-05-12 14:48:29 +0200

    more fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rad.vg',
+  site: 'https://evelynmeow.com',
   integrations: [mdx(), sitemap()],
 
   vite: {
